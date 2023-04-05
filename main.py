40f11daa1a9d8f9bb2cbdd15080f7ed43893e0f7
Dmitry Solovey <73910760+dmnemch@users.noreply.github.com>
2023-04-05 10:45:13 +0300

    Update and rename test.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# https://www.youtube.com/watch?v=qgvmJTmJIKs
+# The TED riddle for inspiration: https://www.youtube.com/watch?v=qgvmJTmJIKs
 
 import random
 import plotly.express as px
@@ -8,6 +8,7 @@ import os
 import subprocess
 import numpy as np
 
+
 def get_args():
     parser = argparse.ArgumentParser(
         prog="strange_numbers",
@@ -15,13 +16,18 @@ def get_args():
     parser.add_argument('-m', '--max', help="maximum number to iterate to")
     return parser.parse_args()
 
-def get_rand_color(): # тупанул, оно не нужно
+
+def get_rand_color(): # actually I don't use this function :)
     col = "#"
     for i in range(6):
         col += random.choice("0123456789ABCDEF")
     return col
 
+
 def generate_df(max):
+    """
+    Generates scratch of df (like in the TED video), then modifided by het 
+    """
     i = 3
     _ = pd.DataFrame([[1, 2, 3], [2, 3, 6]])
     while i * 3 < max:
@@ -29,7 +35,7 @@ def generate_df(max):
             next_row = pd.DataFrame([i, int(_.loc[_[1] == i][2]), i * 3]).T
             _ = pd.concat([_, next_row], ignore_index=True)
 
-            # заполнить во втором столбце нули значениями
+            # this peace of func fills in the gaps in previous rows
             if _[1][i-2] == 0:
                 index = i - 2
                 while _[1][index] == 0:
@@ -46,7 +52,11 @@ def generate_df(max):
         i += 1
     return _
 
+
 def get_lines(df):
+    """
+    Gets lines to color them in one
+    """
     lines = [[]]
     for x in df.T:
         if x + 1 not in np.concatenate(lines, axis=0):
@@ -61,7 +71,11 @@ def get_lines(df):
             continue
     return lines[1::]
 
+
 def get_func_df(df, max):
+    """
+    Creates df with x, y and color group based on get_lines func
+    """
     func_df = pd.DataFrame()
     for x in df.T:
         next_row = pd.DataFrame([df.T[x][0], df.T[x][1]]).T
@@ -80,7 +94,11 @@ def get_func_df(df, max):
     func_df = func_df[func_df['x'] < max//3]
     return func_df
 
+
 def save_plotly(fig, file="out.html"):
+    """
+    Saves *fig* to *file* (destination) and opens it in default html app
+    """
     fig.write_html(file)
     try:
         os.startfile(file)
@@ -90,6 +108,7 @@ def save_plotly(fig, file="out.html"):
         except:
             raise Exception('Could not open file')
 
+            
 def main():
     args = get_args()
     max = int(args.max) * 3
